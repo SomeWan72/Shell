1d9aa7753fe01dcff296d6ba867d30ec288c4d66
Wan Garcia <jugr98@gmail.com>
2018-05-17 11:25:07 +0200

    Add files via upload
    Added task 2

--- Project/Shell_project.c
+++ Project/Shell_project.c
@@ -15,8 +15,38 @@ To compile and run the program:
 **/
 
 #include "job_control.h"   // remember to compile with module job_control.c
+#include <string.h>
 
 #define MAX_LINE 256 /* 256 chars per line, per command, should be enough. */
+
+void cd(char *args[])
+{
+    if(args[1] == NULL)
+    {
+        printf("ERROR, no directory found.\n");
+    }else if(args[2] != NULL)
+    {
+        printf("ERROR with the directory name format.\n");
+    } else
+    {
+        if(chdir(args[1]) == 0)
+        {
+            if(strcmp(args[1], "..") == 0)
+            {
+                printf("Returning to the previous directory.\n");
+            } else
+            {
+                printf("Accessing to the directory %s.\n", args[1]);
+            }
+
+        } else
+        {
+            printf("ERROR, directory %s not found.\n", args[1]);
+        }
+    }
+
+
+}
 
 // -----------------------------------------------------------------------
 //                            MAIN
@@ -33,6 +63,8 @@ int main(void)
     enum status status_res; /* status processed by analyze_status() */
     int info;				/* info processed by analyze_status() */
 
+    ignore_terminal_signals();
+
     while (1)   /* Program terminates normally inside get_command() after ^D is typed*/
     {
         printf("COMMAND->");
@@ -41,32 +73,46 @@ int main(void)
 
         if(args[0]==NULL) continue;   // if empty command
 
-        pid_fork = fork();
+       if(strcmp(args[0], "cd") == 0)
+       {
+           cd(args);
+       } else
+       {
+           pid_fork = fork();
 
-        if(pid_fork < 0)
-        {
-            printf("ERROR trying to fork.");
-            exit(1);
-        } else if(pid_fork == 0)
-        {
-            execvp(args[0], args);
-            printf("ERROR, command %s not found.\n", args[0]);
-            exit(1);
-        } else
-        {
-            if(background == 0)
-            {
-                waitpid(pid_fork, &status, 0);
+           if(pid_fork < 0)
+           {
+               printf("ERROR trying to fork.\n");
+               exit(1);
+           } else if(pid_fork == 0)
+           {
+               new_process_group(getpid());
 
-                if(WEXITSTATUS(status) == 0)
-                {
-                    status_res = analyze_status(waitpid(pid_wait, &status, 0), &info);
-                    printf("Foreground pid: %d, command: %s, %d, info: %d\n", pid_fork, args[0], status_res, info);
-                } else
-                {
-                    printf("Background job running... pid: %d command: %s\n", pid_fork, args[0]);
-                }
-            }
-        }
+               if(background != 1)
+               {
+                   set_terminal(getpid());
+               }
+
+               restore_terminal_signals();
+               execvp(args[0], args);
+               printf("ERROR, command %s not found.\n", args[0]);
+               exit(1);
+           } else
+           {
+               if(background != 1)
+               {
+                   set_terminal(pid_fork);
+                   pid_wait = waitpid(pid_fork, &status, WUNTRACED);
+                   status_res = analyze_status(status, &info);
+                   set_terminal(getpid());
+                   printf("Foreground pid: %d, command: %s, %d, info: %d\n", pid_fork, args[0], status_res, info);
+
+               } else
+               {
+                   printf("Background job running... pid: %d command: %s\n", pid_fork, args[0]);
+               }
+
+           }
+       }
     }
 }
