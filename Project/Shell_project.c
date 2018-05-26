b25551b9f86eb68c6f30e8695be0ce81d38fe5f8
Wan Garcia <jugr98@gmail.com>
2018-05-26 23:30:46 +0200

    Add files via upload
    Added final task (wrong addition before).

--- Project/Shell_project.c
+++ Project/Shell_project.c
@@ -19,7 +19,9 @@ To compile and run the program:
 
 #define MAX_LINE 256 /* 256 chars per line, per command, should be enough. */
 
-void cd(char *args[])
+job* list;
+
+static void cd(char *args[])
 {
     if(args[1] == NULL)
     {
@@ -44,8 +46,111 @@ void cd(char *args[])
             printf("ERROR, directory %s not found.\n", args[1]);
         }
     }
+}
 
+static void bg(char* args[])
+{
+    int num;
 
+    if(list->next == NULL)
+    {
+        printf("ERROR, no processes suspended or in background.\n");
+    } else
+    {
+        if(args[1] != NULL)
+        {
+            num = atoi(args[1]);
+        } else{
+            num = 1;
+        }
+
+        job* plist = get_item_bypos(list, num);
+        plist->state = BACKGROUND;
+        killpg(plist->pgid, SIGCONT);
+    }
+}
+
+static void fg(char* args[])
+{
+    int num, pid_wait, status;
+
+    if(list->next == NULL)
+    {
+        printf("ERROR, no processes suspended or in background.\n");
+    } else
+    {
+        if(args[1] == NULL)
+        {
+            num = atoi(args[1]);
+        } else{
+            num = 1;
+        }
+
+        job* plist = get_item_bypos(list, num);
+        set_terminal(plist->pgid);
+        plist->state = FOREGROUND;
+        killpg(list->pgid, SIGCONT);
+        pid_wait = waitpid(plist->pgid, &status, WUNTRACED);
+
+        if(pid_wait == plist->pgid)
+        {
+            printf("Process %s has finished.\n", plist->command);
+            delete_job(list, plist);
+        }
+
+        set_terminal(getpid());
+    }
+}
+
+static void jobs()
+{
+    if(empty_list(list) != 1)
+    {
+        print_job_list(list);
+    } else
+    {
+        printf("The list is empty.\n");
+    }
+}
+
+static void handler ()
+{
+    enum status status_res;
+    int exStat, info, hasToBeDeleted;
+    pid_t pid_aux;
+    job* plist = list->next;
+
+    while(plist != NULL)
+    {
+        hasToBeDeleted = 0;
+        pid_aux = waitpid(plist->pgid, &exStat, WNOHANG | WUNTRACED);
+
+        if(pid_aux == plist->pgid)
+        {
+            status_res = analyze_status(exStat, &info);
+
+            if(status_res != SUSPENDED)
+            {
+                hasToBeDeleted = 1;
+                printf("The process %d has finished successfully.\n", plist->pgid);
+            } else
+            {
+                printf("The process %d is suspended.\n", plist->pgid);
+                plist->state = STOPPED;
+            }
+
+        }
+
+        if(hasToBeDeleted)
+        {
+            struct job_* jobToDelete = plist;
+            plist = plist->next;
+            delete_job(list, jobToDelete);
+        } else
+        {
+            plist = plist->next;
+        }
+    }
 }
 
 // -----------------------------------------------------------------------
@@ -54,30 +159,48 @@ void cd(char *args[])
 
 int main(void)
 {
+    list = new_list("Jobs");
+
     char inputBuffer[MAX_LINE]; /* buffer to hold the command entered */
     int background;             /* equals 1 if a command is followed by '&' */
     char *args[MAX_LINE/2];     /* command line (of 256) has max of 128 arguments */
+
     // probably useful variables:
+
     int pid_fork, pid_wait; /* pid for created and waited process */
     int status;             /* status returned by wait */
     enum status status_res; /* status processed by analyze_status() */
-    int info;				/* info processed by analyze_status() */
+    int info;   			/* info processed by analyze_status() */
 
     ignore_terminal_signals();
 
     while (1)   /* Program terminates normally inside get_command() after ^D is typed*/
     {
+        signal(SIGCHLD, handler);
         printf("COMMAND->");
         fflush(stdout);
         get_command(inputBuffer, MAX_LINE, args, &background);  /* get next command */
 
-        if(args[0]==NULL) continue;   // if empty command
+        if(args[0]==NULL)
+        {
+            continue;   // if empty command
+        }
 
        if(strcmp(args[0], "cd") == 0)
        {
            cd(args);
+       } else if(strcmp(args[0], "jobs") == 0)
+       {
+           jobs();
+       } else if(strcmp(args[0], "fg") == 0)
+       {
+           fg(args);
+       } else if(strcmp(args[0], "bg") == 0)
+       {
+           bg(args);
        } else
        {
+           block_SIGCHLD();
            pid_fork = fork();
 
            if(pid_fork < 0)
@@ -86,32 +209,41 @@ int main(void)
                exit(1);
            } else if(pid_fork == 0)
            {
+               unblock_SIGCHLD();
                new_process_group(getpid());
 
-               if(background != 1)
+               if(!background)
                {
                    set_terminal(getpid());
                }
 
                restore_terminal_signals();
                execvp(args[0], args);
-               printf("ERROR, command %s not found.\n", args[0]);
                exit(1);
            } else
            {
-               if(background != 1)
-               {
+               if(!background) {
                    set_terminal(pid_fork);
                    pid_wait = waitpid(pid_fork, &status, WUNTRACED);
                    status_res = analyze_status(status, &info);
                    set_terminal(getpid());
-                   printf("Foreground pid: %d, command: %s, %d, info: %d\n", pid_fork, args[0], status_res, info);
+
+                   if (info != 1) {
+                       printf("Foreground pid: %d, command: %s, %d, info: %d\n", pid_fork, args[0], status_res, info);
+                   }
+
+                   if (status_res == SUSPENDED) {
+                       add_job(list, new_job(pid_fork, args[0], STOPPED));
+                       printf("Process (pid: %d, command: %s) has been suspended.\n", getpid(), args[0]);
+                       unblock_SIGCHLD();
+                   }
 
                } else
                {
+                   add_job(list, new_job(pid_fork, args[0], BACKGROUND));
                    printf("Background job running... pid: %d command: %s\n", pid_fork, args[0]);
+                   unblock_SIGCHLD();
                }
-
            }
        }
     }
